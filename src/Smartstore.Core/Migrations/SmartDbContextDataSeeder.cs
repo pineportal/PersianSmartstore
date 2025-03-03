1b613828e2ec461cdf3f93a2472ae26e5549ab4a
pineportal <pineportal@hotmail.com>
2025-03-03 15:22:02 +0330

    fa resources (in progress)

--- src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
+++ src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
@@ -1,4 +1,4 @@
-using Smartstore.Core.Common.Configuration;
+ï»¿using Smartstore.Core.Common.Configuration;
 using Smartstore.Core.Configuration;
 using Smartstore.Core.Security;
 using Smartstore.Data.Migrations;
@@ -24,53 +24,54 @@ namespace Smartstore.Core.Data.Migrations
 
         public void MigrateLocaleResources(LocaleResourcesBuilder builder)
         {
-            
+
 
             builder.AddOrUpdate("Admin.Common.RestartHint",
-                "Changes to the following settings only take effect after the application has been restarted.",
-                "Änderungen an den folgenden Einstellungen werden erst nach einem Neustart der Anwendung wirksam.");
+      "Changes to the following settings only take effect after the application has been restarted.",
+      "Ã„nderungen an den folgenden Einstellungen werden erst nach einem Neustart der Anwendung wirksam.",
+      "ØªØºÛŒÛŒØ±Ø§Øª Ø¯Ø± ØªÙ†Ø¸ÛŒÙ…Ø§Øª Ø²ÛŒØ± ØªÙ†Ù‡Ø§ Ù¾Ø³ Ø§Ø² Ø±Ø§Ù‡â€ŒØ§Ù†Ø¯Ø§Ø²ÛŒ Ù…Ø¬Ø¯Ø¯ Ø¨Ø±Ù†Ø§Ù…Ù‡ Ø§Ø¹Ù…Ø§Ù„ Ù…ÛŒâ€ŒØ´ÙˆÙ†Ø¯.");
 
             #region Performance settings
 
             var prefix = "Admin.Configuration.Settings.Performance";
 
             builder.AddOrUpdate($"{prefix}", "Performance", "Leistung");
-            builder.AddOrUpdate($"{prefix}.Resiliency", "Overload protection", "Überlastungsschutz");
+            builder.AddOrUpdate($"{prefix}.Resiliency", "Overload protection", "Ãœberlastungsschutz");
             builder.AddOrUpdate($"{prefix}.Cache", "Cache", "Cache");
 
             builder.AddOrUpdate($"{prefix}.Hint",
                 "For technically experienced users only. Pay attention to the CPU and memory usage when changing these settings.",
-                "Nur für technisch erfahrene Benutzer. Achten Sie auf die CPU- und Speicherauslastung, wenn Sie diese Einstellungen ändern.");
+                "Nur fÃ¼r technisch erfahrene Benutzer. Achten Sie auf die CPU- und Speicherauslastung, wenn Sie diese Einstellungen Ã¤ndern.");
 
             builder.AddOrUpdate($"{prefix}.CacheSegmentSize",
                 "Cache segment size", 
-                "Cache Segment Größe",
+                "Cache Segment GrÃ¶ÃŸe",
                 "The number of entries in a single cache segment when greedy loading is disabled. The larger the catalog, the smaller this value should be. We recommend segment size of 500 for catalogs with less than 100.000 items.",
-                "Die Anzahl der Einträge in einem einzelnen Cache-Segment, wenn Greedy Loading deaktiviert ist. Je größer der Katalog ist, desto kleiner sollte dieser Wert sein. Wir empfehlen eine Segmentgröße von 500 für Kataloge mit weniger als 100.000 Einträgen.");
+                "Die Anzahl der EintrÃ¤ge in einem einzelnen Cache-Segment, wenn Greedy Loading deaktiviert ist. Je grÃ¶ÃŸer der Katalog ist, desto kleiner sollte dieser Wert sein. Wir empfehlen eine SegmentgrÃ¶ÃŸe von 500 fÃ¼r Kataloge mit weniger als 100.000 EintrÃ¤gen.");
 
             builder.AddOrUpdate($"{prefix}.AlwaysPrefetchTranslations",
                 "Always prefetch translations",
-                "Übersetzungen immer vorladen (Prefetch)",
+                "Ãœbersetzungen immer vorladen (Prefetch)",
                 "By default, only Instant Search prefetches translations. All other product listings work against the segmented cache. For very large multilingual catalogs (> 500,000), enabling this can improve query performance and reduce resource usage.",
-                "Standardmäßig werden nur bei der Sofortsuche Übersetzungen vorgeladen. Alle anderen Produktauflistungen arbeiten mit dem segmentierten Cache. Bei sehr großen mehrsprachigen Katalogen (> 500.000) kann die Aktivierung dieser Option die Abfrageleistung verbessern und die Ressourcennutzung verringern.");
+                "StandardmÃ¤ÃŸig werden nur bei der Sofortsuche Ãœbersetzungen vorgeladen. Alle anderen Produktauflistungen arbeiten mit dem segmentierten Cache. Bei sehr groÃŸen mehrsprachigen Katalogen (> 500.000) kann die Aktivierung dieser Option die Abfrageleistung verbessern und die Ressourcennutzung verringern.");
 
             builder.AddOrUpdate($"{prefix}.AlwaysPrefetchUrlSlugs",
                 "Always prefetch URL slugs",
                 "URL Slugs immer vorladen  (Prefetch)",
                 "By default, only Instant Search prefetches URL slugs. All other product listings work against the segmented cache. For very large multilingual catalogs (> 500,000), enabling this can improve query performance and reduce resource usage.",
-                "Standardmäßig werden nur bei der Sofortsuche URL slugs vorgeladen. Alle anderen Produktauflistungen arbeiten mit dem segmentierten Cache. Bei sehr großen mehrsprachigen Katalogen (> 500.000) kann die Aktivierung dieser Option die Abfrageleistung verbessern und die Ressourcennutzung verringern.");
+                "StandardmÃ¤ÃŸig werden nur bei der Sofortsuche URL slugs vorgeladen. Alle anderen Produktauflistungen arbeiten mit dem segmentierten Cache. Bei sehr groÃŸen mehrsprachigen Katalogen (> 500.000) kann die Aktivierung dieser Option die Abfrageleistung verbessern und die Ressourcennutzung verringern.");
 
             builder.AddOrUpdate($"{prefix}.MaxUnavailableAttributeCombinations",
                 "Max. unavailable attribute combinations",
-                "Max. nicht verfügbare Attributkombinationen",
+                "Max. nicht verfÃ¼gbare Attributkombinationen",
                 "Maximum number of attribute combinations that will be loaded and parsed to make them unavailable for selection on the product detail page.",
-                "Maximale Anzahl von Attributkombinationen, die geladen und analysiert werden, um nicht verfügbare Kombinationen zu ermitteln.");
+                "Maximale Anzahl von Attributkombinationen, die geladen und analysiert werden, um nicht verfÃ¼gbare Kombinationen zu ermitteln.");
 
             builder.AddOrUpdate($"{prefix}.MediaDupeDetectorMaxCacheSize",
                 "Media Duplicate Detector max. cache size",
-                "Max. Cache-Größe für Medien-Duplikat-Detektor",
+                "Max. Cache-GrÃ¶ÃŸe fÃ¼r Medien-Duplikat-Detektor",
                 "Maximum number of MediaFile entities to cache for duplicate file detection. If a media folder contains more files, no caching is done for scalability reasons and the MediaFile entities are loaded directly from the database.",
-                "Maximale Anzahl der MediaFile-Entitäten, die für die Duplikat-Erkennung zwischengespeichert werden. Enthält ein Medienordner mehr Dateien, erfolgt aus Gründen der Skalierbarkeit keine Zwischenspeicherung und die MediaFile-Entitäten werden direkt aus der Datenbank geladen.");
+                "Maximale Anzahl der MediaFile-EntitÃ¤ten, die fÃ¼r die Duplikat-Erkennung zwischengespeichert werden. EnthÃ¤lt ein Medienordner mehr Dateien, erfolgt aus GrÃ¼nden der Skalierbarkeit keine Zwischenspeicherung und die MediaFile-EntitÃ¤ten werden direkt aus der Datenbank geladen.");
 
             prefix = "Admin.Configuration.Settings.Resiliency";
 
@@ -78,19 +79,19 @@ namespace Smartstore.Core.Data.Migrations
                 @"Overload protection is used to keep server resources under control, prevent latencies from getting out of hand and keep the system performant and available 
 in the event of increased traffic (e.g. due to unidentifiable ""Bad Bots"", peaks, sales events, sudden high visitor numbers).
 Limits only apply to guest accounts and bots, not to registered users.",
-                @"Überlastungsschutz dient dazu, die Server-Ressourcen unter Kontrolle zu halten, Latenzen nicht ausufern zu lassen und im Falle von erhöhtem Ansturm 
-(z.B. durch nicht identifizierbare ""Bad-Bots"", Peaks, Sales Events, plötzlich hohe Nutzerzahlen) das System performant und verfügbar zu halten.
-Limits gelten nur für Gastkonten und Bots, nicht für registrierte User.");
+                @"Ãœberlastungsschutz dient dazu, die Server-Ressourcen unter Kontrolle zu halten, Latenzen nicht ausufern zu lassen und im Falle von erhÃ¶htem Ansturm 
+(z.B. durch nicht identifizierbare ""Bad-Bots"", Peaks, Sales Events, plÃ¶tzlich hohe Nutzerzahlen) das System performant und verfÃ¼gbar zu halten.
+Limits gelten nur fÃ¼r Gastkonten und Bots, nicht fÃ¼r registrierte User.");
 
             builder.AddOrUpdate($"{prefix}.LongTraffic", "Traffic limit", "Besucherlimit");
             builder.AddOrUpdate($"{prefix}.LongTrafficNotes",
                 "Configuration of the long traffic window. Use these settings to monitor traffic restrictions over a longer period of time, such as a minute or longer.",
-                "Konfiguration des langen Zeitfensters. Verwenden Sie diese Einstellungen, um Limits über einen längeren Zeitraum zu überwachen, z.B. eine Minute oder länger.");
+                "Konfiguration des langen Zeitfensters. Verwenden Sie diese Einstellungen, um Limits Ã¼ber einen lÃ¤ngeren Zeitraum zu Ã¼berwachen, z.B. eine Minute oder lÃ¤nger.");
 
             builder.AddOrUpdate($"{prefix}.PeakTraffic", "Peak", "Lastspitzen");
             builder.AddOrUpdate($"{prefix}.PeakTrafficNotes",
                 "The peak traffic window defines the shorter period used for detecting sudden traffic spikes. These settings are useful for reacting to bursts of traffic that occur in a matter of seconds.",
-                "Den kürzere Zeitraum, der für die Erkennung plötzlicher Lastspitzen verwendet wird. Diese Einstellungen sind nützlich, um auf Lastspitzen zu reagieren, die innerhalb weniger Sekunden auftreten.");
+                "Den kÃ¼rzere Zeitraum, der fÃ¼r die Erkennung plÃ¶tzlicher Lastspitzen verwendet wird. Diese Einstellungen sind nÃ¼tzlich, um auf Lastspitzen zu reagieren, die innerhalb weniger Sekunden auftreten.");
 
             builder.AddOrUpdate($"{prefix}.TrafficTimeWindow",
                 "Time window",
@@ -100,7 +101,7 @@ Limits gelten nur für Gastkonten und Bots, nicht für registrierte User.");
 
             builder.AddOrUpdate($"{prefix}.TrafficLimitGuest",
                 "Guest limit",
-                "Gäste-Grenzwert",
+                "GÃ¤ste-Grenzwert",
                 "The maximum number of requests allowed from guest users within the duration of the defined time window. Empty value means there is no limit applied for guest users.",
                 "Die maximale Anzahl von Gastbenutzern innerhalb des festgelegten Zeitfensters. Ein leerer Wert bedeutet: keine Begrenzung.");
 
@@ -117,30 +118,30 @@ Limits gelten nur für Gastkonten und Bots, nicht für registrierte User.");
 It ensures that the overall system load remains within acceptable thresholds, regardless of the distribution of requests among specific user types.  
 Unlike guest or bot limiters, this global limit acts as a safeguard for the entire system. If the cumulative requests from both types exceed this limit 
 within the observation window, additional requests may be denied, even if type-specific limits have not been reached. An empty value means that there is no global limit.",
-                @"Das globale Limit für Gäste und Bots zusammen. Dieses Limit gilt für den kombinierten Traffic von Gästen und Bots. 
-Es stellt sicher, dass die Gesamtsystemlast innerhalb akzeptabler Schwellenwerte bleibt, unabhängig von der Verteilung der Anfragen auf bestimmte Benutzertypen. 
-Im Gegensatz zu Gast- oder Bot-Limitern dient dieses globale Limit als Schutz für das gesamte System. 
-Wenn die kumulierten Anfragen beider Typen dieses Limit innerhalb des Beobachtungsfensters überschreiten, werden weitere Anfragen abgelehnt, 
+                @"Das globale Limit fÃ¼r GÃ¤ste und Bots zusammen. Dieses Limit gilt fÃ¼r den kombinierten Traffic von GÃ¤sten und Bots. 
+Es stellt sicher, dass die Gesamtsystemlast innerhalb akzeptabler Schwellenwerte bleibt, unabhÃ¤ngig von der Verteilung der Anfragen auf bestimmte Benutzertypen. 
+Im Gegensatz zu Gast- oder Bot-Limitern dient dieses globale Limit als Schutz fÃ¼r das gesamte System. 
+Wenn die kumulierten Anfragen beider Typen dieses Limit innerhalb des Beobachtungsfensters Ã¼berschreiten, werden weitere Anfragen abgelehnt, 
 auch wenn die typspezifischen Limits nicht erreicht wurden. Ein leerer Wert bedeutet: keine Begrenzung.");
 
             builder.AddOrUpdate($"{prefix}.EnableOverloadProtection",
                 "Enable overload protection",
-                "Überlastungsschutz aktivieren",
+                "Ãœberlastungsschutz aktivieren",
                 "When enabled, the system applies the defined traffic limits and overload protection policies. If disabled, no traffic limits are enforced.",
                 "Wendet die festgelegten Richtlinien an. Wenn diese Option deaktiviert ist, werden keine Begrenzungen erzwungen.");
 
             builder.AddOrUpdate($"{prefix}.ForbidNewGuestsIfSubRequest",
                 "If sub request, forbid \"new\" guests",
-                "Wenn Sub-Request, \"neue\" Gäste blockieren",
+                "Wenn Sub-Request, \"neue\" GÃ¤ste blockieren",
                 @"Forbids ""new"" guest users if the request is a sub/secondary request, e.g., an AJAX request, POST, script, media file, etc. This setting can be used to restrict 
 the creation of new guest sessions on successive (secondary) resource requests. A ""bad bot"" that does not accept cookies is difficult to identify 
 as a bot and may create a new guest session with each (sub)-request, especially if it varies its client IP address and user agent string with each request. 
 If enabled, new guests will be blocked under these circumstances.",
-                @"Blockiert ""neue"" Gastbenutzer, wenn es sich bei der Anforderung um eine untergeordnete/sekundäre Anforderung handelt, z. B. AJAX, POST, Skript, Mediendatei usw. 
-Diese Einstellung kann verwendet werden, um die Erstellung neuer Gastsitzungen bei aufeinander folgenden (sekundären) Ressourcenanfragen einzuschränken. 
+                @"Blockiert ""neue"" Gastbenutzer, wenn es sich bei der Anforderung um eine untergeordnete/sekundÃ¤re Anforderung handelt, z. B. AJAX, POST, Skript, Mediendatei usw. 
+Diese Einstellung kann verwendet werden, um die Erstellung neuer Gastsitzungen bei aufeinander folgenden (sekundÃ¤ren) Ressourcenanfragen einzuschrÃ¤nken. 
 Ein ""Bad Bot"", der keine Cookies akzeptiert, ist schwer als Bot zu erkennen und kann bei jeder (Unter-)Anfrage eine neue Gastsitzung erzeugen, 
-insbesondere wenn er seine Client-IP-Adresse und den User-Agent-String bei jeder Anfrage ändert. 
-Wenn diese Option aktiviert ist, werden neue Gäste unter diesen Umständen blockiert.");
+insbesondere wenn er seine Client-IP-Adresse und den User-Agent-String bei jeder Anfrage Ã¤ndert. 
+Wenn diese Option aktiviert ist, werden neue GÃ¤ste unter diesen UmstÃ¤nden blockiert.");
 
             #endregion
 
@@ -148,7 +149,7 @@ Wenn diese Option aktiviert ist, werden neue Gäste unter diesen Umständen blocki
 			
 			builder.AddOrUpdate("Admin.Configuration.Settings.Shipping.CalculateShippingAtCheckout",
                 "Calculate shipping costs during checkout",
-                "Versandkosten während des Checkouts berechnen",
+                "Versandkosten wÃ¤hrend des Checkouts berechnen",
                 "Specifies whether shipping costs are displayed on the shopping cart page as long as the customer has not yet entered a shipping address. If activated, a note appears instead that the calculation will only take place at checkout.",
                 "Legt fest, ob Versandkosten auf der Warenkorbseite angezeigt werden, solange der Kunde noch keine Lieferanschrift eingegeben hat. Wenn aktiviert, erscheint stattdessen ein Hinweis, dass die Berechnung erst beim Checkout erfolgt.");
         }
